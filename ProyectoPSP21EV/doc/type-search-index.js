c1648afd413bd86eba16b7d228c73e2bac6275d2
Jonatandlfuente <60443605+Jonatandlfuente@users.noreply.github.com>
2020-11-26 17:32:11 +0100

    Add files via upload

--- ProyectoPSP21EV/doc/type-search-index.js
+++ ProyectoPSP21EV/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"controllers","l":"BBddControllers"},{"p":"models","l":"Hilo"},{"p":"controllers","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"controllers","l":"BBddControllers"},{"p":"models","l":"Hilo"},{"p":"models","l":"Hilos"},{"p":"controllers","l":"Main"}]
